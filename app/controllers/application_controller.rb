86a949a2429382f44f5099585072f0f329d12a8d
Isidora <iaramirez@miuandes.cl>
2019-05-16 18:10:20 -0400

    voh dale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,24 @@ class ApplicationController < ActionController::Base
   def index
 
   end
-
+  helper_method :current_user
   def current_user
 	#complete this method
     return unless session[:user_id]
     @current_user ||= User.find(session[:user_id])
-  end
-
-  def is_user_logged_in?
-	#complete this method
-  	logged_in = false
-	if logged_in then true else redirect_to root_path end 
+    user_name = User.find(session[:user_id])
   end
   helper_method :is_user_logged_in?
+  def is_user_logged_in?
+	#complete this method
+  logged_in = false
+  if cookies["user_id"] != ""
+    logged_in = true
+  end
+  if self.current_user
+    logged_in = true
+  end
+	if logged_in then true else redirect_to root_path end 
+  end
+  
 end
